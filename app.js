d9815ee9a8e92e8777e10116e2da2334ec8ac80c
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-05-05 17:44:15 +0300

    eklemeler yapıldı

---
+++ app.js
@@ -0,0 +1,33 @@
+const topcuVeri = [
+    {
+        id:1,
+        name: 'Osvaldo',
+        img: 'https://randomplayers-test1.s3.eu-central-1.amazonaws.com/europe/seriea2010/osvaldo.jpg',
+        sıkA:'Nani',
+        sıkB:'Osvaldo',
+        sıkC:'Pirlo',
+        sıkD:'Seedorf'
+    },
+    {
+        id:2,
+        name: 'Messi',
+        img: 'https://cdn.ntvspor.net/f3bf565289384a76abdf7e271b2e0e50.jpg?mode=crop&w=940&h=626',
+        sıkA:'Ronaldo',
+        sıkB:'Delgado',
+        sıkC:'Levandowski',
+        sıkD:'Messi'
+    }
+];
+
+
+
+
+
+
+/*function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+    */
