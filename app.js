9dc2c2dbe5466dda4bae1805846409fc40341990
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-05-05 18:17:09 +0300

    rastgele secim eklendi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const topcuVeri = [
     {
         id:1,
-        name: 'Osvaldo',
+        name:'Osvaldo',
         img: 'https://randomplayers-test1.s3.eu-central-1.amazonaws.com/europe/seriea2010/osvaldo.jpg',
         sıkA:'Nani',
         sıkB:'Osvaldo',
@@ -10,19 +10,36 @@ const topcuVeri = [
     },
     {
         id:2,
-        name: 'Messi',
-        img: 'https://cdn.ntvspor.net/f3bf565289384a76abdf7e271b2e0e50.jpg?mode=crop&w=940&h=626',
+        name:'Messi',
+        img: 'https://iaftm.tmgrup.com.tr/a30a55/750/373/0/0/632/314?u=https://iftm.tmgrup.com.tr/2019/03/07/dunya-sokta-messi-barcelonadan-ayriliyor-1551950108348.jpg',
         sıkA:'Ronaldo',
         sıkB:'Delgado',
         sıkC:'Levandowski',
         sıkD:'Messi'
-    }
+    },
+    {
+        id:3,
+        name:'Totti',
+        img: 'https://quotepark.com/media/authors/francesco-totti.300w.webp',
+        sıkA:'Puyol',
+        sıkB:'Zidane',
+        sıkC:'Saka',
+        sıkD:'Totti'
+    },
 ];
 
+const sıklar = document.getElementsByClassName('button-container');
 
+function rastgeleFutbolcuSec() {
+    const rastgeleIndex = Math.floor(Math.random() * topcuVeri.length);
+    const secilenTopcu = topcuVeri[rastgeleIndex];
+    const imgElement = document.getElementById("topcuImg");
+    imgElement.src = secilenTopcu.img;
+  
+    return secilenTopcu;
+  };
 
-
-
+  rastgeleFutbolcuSec()
 
 /*function showPerson(person) {
     const item = reviews[person];
